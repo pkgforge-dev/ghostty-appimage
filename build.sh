8f949d3ff2c0cc3eba31083df5f6b5f73be68481
psadi <mail@adithyaps.xyz>
2025-09-01 22:04:42 +0530

    refactor: Implement pkgforge scripts
    - Utilize debloated scripts for installing OpenGL, GTK4 and libxml.
    - Delegate tasks to quick-sharun and uruntime2appimage.
    - Modify generics to support script defaults, directories, environment
    variables and cleanup.

--- build.sh
+++ build.sh
@@ -4,28 +4,22 @@ set -eux
 
 ARCH="$(uname -m)"
 GHOSTTY_VERSION="$(cat VERSION)"
-TMP_DIR="/tmp/ghostty-build"
-APP_DIR="${TMP_DIR}/ghostty.AppDir"
 PUB_KEY="RWQlAjJC23149WL2sEpT/l0QKy7hMIFhYdQOFy0Z7z7PbneUgvlsnYcV"
-UPINFO="gh-releases-zsync|$(echo "${GITHUB_REPOSITORY}" | tr '/' '|')|latest|Ghostty-*$ARCH.AppImage.zsync"
-APPDATA_FILE="${PWD}/assets/ghostty.appdata.xml"
-DESKTOP_FILE="${PWD}/assets/ghostty.desktop"
-LIBS2BUNDLE="./bin/ghostty /usr/lib/libEGL* /usr/lib/gdk-pixbuf-*/*/*/*"
+
+export ADD_HOOKS="self-updater.bg.hook"
+export UPINFO="gh-releases-zsync|$(echo "${GITHUB_REPOSITORY}" | tr '/' '|')|latest|Ghostty-*$ARCH.AppImage.zsync"
+export URUNTIME_PRELOAD=1
+export DEPLOY_OPENGL=1
+export EXEC_WRAPPER=1
+
 BUILD_ARGS="
 	--summary all \
-	--prefix ${APP_DIR} \
 	-Doptimize=ReleaseFast \
 	-Dcpu=baseline \
 	-Dpie=true \
 	-Demit-docs \
 	-Dgtk-wayland=true \
 	-Dgtk-x11=true"
-
-rm -rf "${TMP_DIR}"
-
-mkdir -p -- "${TMP_DIR}" "${APP_DIR}/share/metainfo" "${APP_DIR}/shared/lib"
-
-cd "${TMP_DIR}"
 
 if [ "${GHOSTTY_VERSION}" = "tip" ]; then
 	wget "https://github.com/ghostty-org/ghostty/releases/download/tip/ghostty-source.tar.gz" -O "ghostty-${GHOSTTY_VERSION}.tar.gz"
@@ -45,10 +39,7 @@ tar -xzmf "ghostty-${GHOSTTY_VERSION}.tar.gz"
 rm "ghostty-${GHOSTTY_VERSION}.tar.gz" \
 	"ghostty-${GHOSTTY_VERSION}.tar.gz.minisig"
 
-BUILD_DIR="ghostty-${GHOSTTY_VERSION}"
 BUILD_ARGS="${BUILD_ARGS} -Dversion-string=${GHOSTTY_VERSION}"
-
-cd "${TMP_DIR}/${BUILD_DIR}"
 
 #Fetch Zig Cache
 if [ -f './nix/build-support/fetch-zig-cache.sh' ]; then
@@ -57,64 +48,17 @@ if [ -f './nix/build-support/fetch-zig-cache.sh' ]; then
 fi
 
 # Build Ghostty with zig
-zig build ${BUILD_ARGS}
+(
+	cd "ghostty-${GHOSTTY_VERSION}"
+	zig build ${BUILD_ARGS}
+)
 
-# Prepare AppImage -- Configure launcher script, metainfo and desktop file with icon.
-cd "${APP_DIR}"
+export OUTNAME="Ghostty-${GHOSTTY_VERSION}-${ARCH}.AppImage"
+export DESKTOP="./ghostty-${GHOSTTY_VERSION}/zig-out/share/applications/com.mitchellh.ghostty.desktop"
+export ICON="./ghostty-${GHOSTTY_VERSION}/zig-out/share/icons/hicolor/256x256/apps/com.mitchellh.ghostty.png"
 
-cp "${APPDATA_FILE}" "share/metainfo/com.mitchellh.ghostty.appdata.xml"
-cp "${DESKTOP_FILE}" "share/applications/com.mitchellh.ghostty.desktop"
+./quick-sharun ./ghostty-${GHOSTTY_VERSION}/zig-out/bin/ghostty
+./uruntime2appimage
 
-ln -s "com.mitchellh.ghostty.desktop" "share/applications/ghostty.desktop"
-ln -s "share/applications/com.mitchellh.ghostty.desktop" .
-ln -s "share/icons/hicolor/256x256/apps/com.mitchellh.ghostty.png" .
-ln -s "share/icons/hicolor/256x256/apps/com.mitchellh.ghostty.png" .DirIcon
-
-# bundle all libs
-xvfb-run -a -- sharun l -p -v -e -s -k ${LIBS2BUNDLE}
-
-# preload libpixbufloader /w ld-preload-open as svg icons breaks
-# either on ghostty tab bar or gnome-text-editor while config edit or both :(
-mv ./shared/lib/gdk-pixbuf-2.0 ./
-cp -rv /opt/path-mapping.so ./shared/lib/
-cp -rv gdk-pixbuf-2.0/2.10.0/loaders/libpixbufloader_svg.so ./shared/lib/
-
-echo 'path-mapping.so' >./.preload
-echo 'PATH_MAPPING=/usr/lib/gdk-pixbuf-2.0:${SHARUN_DIR}/gdk-pixbuf-2.0' >>./.env
-
-echo 'GHOSTTY_RESOURCES_DIR=${SHARUN_DIR}/share/ghostty' >>./.env
-echo 'unset ARGV0' >>./.env
-
-ln -s ./bin/ghostty ./AppRun
-./sharun -g
-
-VERSION="$(./AppRun --version | awk 'FNR==1 {print $2}')"
-if [ -z "${VERSION}" ]; then
-	echo "ERROR: Could not get version from ghostty binary"
-	exit 1
-fi
-
-GHOSTTY_APPIMAGE="Ghostty-${VERSION}-${ARCH}.AppImage"
-
-cd "${TMP_DIR}"
-
-# create app image
-cp "$(command -v uruntime)" ./uruntime
-cp "$(command -v uruntime-lite)" ./uruntime-lite
-
-# persist mount for faster launch times
-sed -i 's|URUNTIME_MOUNT=[0-9]|URUNTIME_MOUNT=0|' ./uruntime-lite
-
-# update info
-./uruntime-lite --appimage-addupdinfo "${UPINFO}"
-
-echo "Generating AppImage"
-./uruntime --appimage-mkdwarfs -f \
-	--set-owner 0 --set-group 0 \
-	--no-history --no-create-timestamp \
-	--compression zstd:level=22 -S26 -B8 \
-	--header uruntime-lite -i "${APP_DIR}" \
-	-o "${GHOSTTY_APPIMAGE}"
-
-echo "Generating Zsync file"
-zsyncmake ./*.AppImage -u ./*.AppImage
+mkdir -p ./dist
+mv -v ./*.AppImage* ./dist
