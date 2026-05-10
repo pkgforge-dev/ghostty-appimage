ac2d1e3ca5012d9a3c321fae2e4c98b77f588690
psadi <adithya3494@gmail.com>
2026-05-10 23:39:43 +0530

    revert: add  back

--- bin/bundle-appimage.sh
+++ bin/bundle-appimage.sh
@@ -8,6 +8,7 @@ GHOSTTY_VERSION="$(cat VERSION)"
 export UPINFO="gh-releases-zsync|$(echo "${GITHUB_REPOSITORY}" | tr '/' '|')|latest|Ghostty-*$ARCH.AppImage.zsync"
 export DEPLOY_OPENGL=1
 export EXEC_WRAPPER=1
+export URUNTIME_PRELOAD=1
 export OUTNAME="Ghostty-${GHOSTTY_VERSION}-${ARCH}.AppImage"
 export DESKTOP="./ghostty-${GHOSTTY_VERSION}/zig-out/share/applications/com.mitchellh.ghostty.desktop"
 export ICON="./ghostty-${GHOSTTY_VERSION}/zig-out/share/icons/hicolor/256x256/apps/com.mitchellh.ghostty.png"
