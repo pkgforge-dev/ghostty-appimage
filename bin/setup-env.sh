164c4421df2a0013feab31b7f340a2d582520f1a
Samueru-sama <xdglawyer@outlook.com>
2026-06-16 13:58:04 -0400

    use our setup action

--- bin/setup-env.sh
+++ bin/setup-env.sh
@@ -27,27 +27,17 @@ ARCH="$(uname -m)"
 MINISIGN_VERSION="$(get_latest_gh_release 'jedisct1/minisign')"
 
 GH_BASE="https://github.com"
-GH_USER_CONTENT="https://raw.githubusercontent.com"
 
 MINISIGN_URL="${GH_BASE}/jedisct1/minisign/releases/download/${MINISIGN_VERSION}/minisign-${MINISIGN_VERSION}-linux.tar.gz"
 
-DEBLOATED_PKGS="${GH_USER_CONTENT}/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
-SHARUN="${GH_USER_CONTENT}/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
-
-# Install Debloated Pkgs
-wget "${DEBLOATED_PKGS}" -O /tmp/get-debloated-pkgs.sh
-chmod a+x /tmp/get-debloated-pkgs.sh
-sh /tmp/get-debloated-pkgs.sh --add-common --prefer-nano
+# Install Debloated Pkgs (get-debloated-pkgs provided by anylinux-setup-action)
+get-debloated-pkgs --add-common --prefer-nano
 
 # minisign: https://github.com/jedisct1/minisign
 rm -rf /usr/local/bin/minisign
 wget "${MINISIGN_URL}" -O /tmp/minisign-linux.tar.gz
 tar -xzf /tmp/minisign-linux.tar.gz -C /tmp
 mv /tmp/minisign-linux/"${ARCH}"/minisign /usr/local/bin
-
-# Sharun
-wget "${SHARUN}" -O quick-sharun
-chmod +x quick-sharun
 
 # Cleanup
 pacman -Scc --noconfirm
