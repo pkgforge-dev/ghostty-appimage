87ea00a671a99dca941bb267f839cddbbfa13ef8
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-06-16 14:12:18 -0400

    use our setup action (#149)

--- bin/bundle-appimage.sh
+++ bin/bundle-appimage.sh
@@ -13,9 +13,9 @@ export OUTNAME="Ghostty-${GHOSTTY_VERSION}-${ARCH}.AppImage"
 export DESKTOP="./ghostty-${GHOSTTY_VERSION}/zig-out/share/applications/com.mitchellh.ghostty.desktop"
 export ICON="./ghostty-${GHOSTTY_VERSION}/zig-out/share/icons/hicolor/256x256/apps/com.mitchellh.ghostty.png"
 
-./quick-sharun ./ghostty-${GHOSTTY_VERSION}/zig-out/bin/ghostty
+quick-sharun ./ghostty-${GHOSTTY_VERSION}/zig-out/bin/ghostty
 cp -rf ./ghostty-${GHOSTTY_VERSION}/zig-out/share/* ./AppDir/share/
-./quick-sharun --make-appimage
+quick-sharun --make-appimage
 
 mkdir -p ./dist
 mv -v ./*.AppImage* ./dist
